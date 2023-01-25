9c7ce771caa5a3476c8e3e18fa66173e94e12fdf
Taylor <hillking2017@gmail.com>
2023-01-24 23:27:49 -0800

    audio method added

--- game.js
+++ game.js
@@ -1,15 +1,16 @@
-const gamePattern = [];
-const buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
 
-// this returns a random number between 0 and 3.
 function nextSequence() {
-    return Math.floor(Math.random()*4)
+    // this returns a random number between 0 and 3.
+    var randomNumber = Math.floor(Math.random()*4)
+    // This sets "randomChosenColor" to a random color from the button colors array; this uses the random number to select from the array's index.
+    var randomChosenColor = buttonColors[randomNumber];
+    // Here I am using the "push" array method to add a random color to the "gamePattern" array.
+    gamePattern.push(randomChosenColor);
+    // This makes an animation for the random color generated.
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    // This plays the corresponding audio.
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 };
-// Setting the output of the above function to a variable.
-const randomNumber = nextSequence();
-// This sets "randomChosenColor" to a random color from the button colors array; this uses the random number to select from the array's index.
-const randomChosenColor = buttonColors[randomNumber];
-// Here I am using the "push" array method to add a random color to the "gamePattern" array.
-gamePattern.push(randomChosenColor);
-
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);

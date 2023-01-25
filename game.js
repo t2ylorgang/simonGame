59eb5097e350fb73b54391e035d88cc12e61a59c
Taylor <hillking2017@gmail.com>
2023-01-24 22:16:46 -0800

    added arrays for game and code to add colors at random to an array

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
-// this returns a random number between 0 and 3
+const gamePattern = [];
+const buttonColors = ["red", "blue", "green", "yellow"];
+
+// this returns a random number between 0 and 3.
 function nextSequence() {
     return Math.floor(Math.random()*4)
 };
+// Setting the output of the above function to a variable.
+const randomNumber = nextSequence();
+// This sets "randomChosenColor" to a random color from the button colors array; this uses the random number to select from the array's index.
+const randomChosenColor = buttonColors[randomNumber];
+// Here I am using the "push" array method to add a random color to the "gamePattern" array.
+gamePattern.push(randomChosenColor);

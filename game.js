9c1206ef03672503379a4d822f8f9e8a64251be8
Taylor <hillking2017@gmail.com>
2023-02-14 22:07:19 -0800

    in the process of step 5

--- game.js
+++ game.js
@@ -23,3 +23,28 @@ $(".btn").on("click", function(e) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 });
+
+function playSound(name) {
+    // this returns a random number between 0 and 3.
+    var randomNumber = Math.floor(Math.random()*4)
+    // This sets "randomChosenColor" to a random color from the button colors array; this uses the random number to select from the array's index.
+    var randomChosenColor = buttonColors[randomNumber];
+    // Here I am using the "push" array method to add a random color to the "gamePattern" array.
+    gamePattern.push(randomChosenColor);
+    // This makes an animation for the random color generated.
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    // This plays the corresponding audio.
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+
+    $(".btn").on("click", function(e) {
+        var userChosenColor = e.target.id;
+        var name = e.target.id
+        userClickedPattern.push(userChosenColor);
+        var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+    });
+}
+
+playSound();
+playSound();
